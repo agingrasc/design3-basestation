db9cc39f119e488250b5824c7f33f3203b946caa
Dali-PC\dali <rouge_dali_007@hotmqail.com>
2017-02-07 22:02:02 -0600

    refactor de l'api de la base station

--- server/main.py
+++ server/main.py
@@ -1,11 +1,12 @@
-import requests as req
 import sys
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response
+from basestation.server.api.gotoposition import gotoposition
+
 app = Flask(__name__)
 
-ROBOT_API_URL = "http://localhost:5000/go-to-position"
-PORT = 12345
+app.register_blueprint(gotoposition)
 
+PORT = 12345
 
 @app.after_request
 def after_request(data):
@@ -25,22 +26,6 @@ def bad_request(error):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
-
-
-@app.route("/go-to-position/", methods=['POST'])
-def goto_position():
-    pos_x = request.json["x"]
-    pos_y = request.json["y"]
-    req.post(
-        url= url,
-        json={
-            "x": pos_x,
-            "y": pos_y
-            },
-        )
-    send_response = make_response(jsonify({"x": pos_x, "y": pos_y}), 200)
-    return send_response
-
 
 if __name__ == '__main__':
     url = sys.argv[1]
