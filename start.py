52d06b7e71aeb95bb695d8c868862818eff4f98b
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-03-06 20:09:04 -0500

    make it compatible with windows

--- start.py
+++ start.py
@@ -1,16 +1,23 @@
 import sys
+import os
 import subprocess
 
 url = sys.argv[1]
 
-proc_vision = subprocess.Popen("./server/service/vision.py", shell=True)
-proc_vision_fake = subprocess.Popen(
-    "./server/service/fakevision.py", shell=True)
-proc_main_api = subprocess.Popen("./server/main.py " + url, shell=True)
+proc_vision = subprocess.Popen(["python server/service/vision.py"], shell=True)
 
-proc_install_ui = subprocess.Popen(["cd client;npm install"], shell=True)
-proc_install_ui.communicate()
-proc_ui = subprocess.Popen(["cd client;au run"], shell=True)
+if os.name == 'nt':
+    proc_install_ui = subprocess.Popen(["cd client & npm install"], shell=True)
+    proc_install_ui.communicate()
+    proc_ui = subprocess.Popen(["cd client & au run"], shell=True)
+else:
+    proc_install_ui = subprocess.Popen(["cd client;npm install"], shell=True)
+    proc_install_ui.communicate()
+    proc_ui = subprocess.Popen(["cd client;au run"], shell=True)
+
+proc_vision_fake = subprocess.Popen(
+    ["python server/service/fakevision.py"], shell=True)
+proc_main_api = subprocess.Popen(["python server/main.py" + url], shell=True)
 
 proc_vision_fake.communicate()
 proc_vision.communicate()
