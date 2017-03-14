9ac724c41aebd395210f47496f2f2220b484f1a7
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-03-14 15:22:12 -0400

    add pull_robot_position

--- server/service/vision.py
+++ server/service/vision.py
@@ -9,6 +9,8 @@ GLOBAL = {}
 GLOBAL[visionformat.PULL_VISION_DATA] = ""
 REGISTERS_TO_VISION_DATA = []
 
+ROBOT_POSITION = {}
+
 
 class EchoWebSocket(websocket.WebSocketHandler):
     def initialize(self):
@@ -27,6 +29,8 @@ class EchoWebSocket(websocket.WebSocketHandler):
             pull_vision_data(self)
         if visionformat.REGISTER_VISION_DATA == value[visionformat.HEADERS]:
             register_vision_data(self)
+        if value[visionformat.HEADERS] == "pull_robot_position":
+            self.write_message(ROBOT_POSITION)
 
     def on_close(self):
         if any(self == connection for connection in REGISTERS_TO_VISION_DATA):
@@ -40,6 +44,7 @@ class EchoWebSocket(websocket.WebSocketHandler):
 
 def push_vision_data(connection, value):
     GLOBAL[visionformat.PULL_VISION_DATA] = value[visionformat.DATA]
+    ROBOT_POSITION = value["data"]["world"]["robot"]["position"]
     connection.write_message("Ok")
     for connection in REGISTERS_TO_VISION_DATA:
         connection.write_message(GLOBAL[visionformat.PULL_VISION_DATA])
