e9b5bdda1a9f17d726645107e4bdaee20e623147
jsletellier <js.letellier@gmail.com>
2017-04-03 09:09:07 -0400

    Refatoring, extract methods, make base url formatted string

--- server/api/gotoposition.py
+++ server/api/gotoposition.py
@@ -1,33 +1,36 @@
-import datetime
-
 import numpy as np
 import requests as req
 from flask import jsonify, make_response, request, Blueprint
 
-ROBOT_API_URL = "http://192.168.0.27:8080/go-to-position"
+ROBOT_API_BASE_URL = "http://192.168.0.27:8080{}"
 
 gotoposition = Blueprint('goto_position', __name__)
 
 
+def validate_payload(data):
+    data['destination']['theta'] = string_deg_to_rad(data['destination']['theta'])
+    return data
+
+
+def string_deg_to_rad(string_deg_angle):
+    return str(np.deg2rad(float(string_deg_angle)))
+
+
+def relay_to_robot(endpoint, payload):
+    return req.post(url=ROBOT_API_BASE_URL.format(endpoint), json=payload)
+
+
 @gotoposition.route("/go-to-position", methods=['POST'])
 def goto_position():
-    data = request.json
-
-    data['destination']['theta'] = str(np.deg2rad(float(data['destination']['theta'])))
-    print(data)
-
-    req.post(url=ROBOT_API_URL, json=data)
-    send_response = make_response(jsonify(data), 200)
-    send_response.headers['Access-Control-Allow-Origin'] = "*"
+    payload = validate_payload(request.json)
+    robot_confirm = relay_to_robot("/go-to-position", payload).json()
+    send_response = make_response(jsonify(payload), 200)
     return send_response
 
 
 @gotoposition.route('/go-to-pathfinder', methods=["POST"])
 def go_to_pathfinder():
-    data = request.json
-    data['destination']['theta'] = str(np.deg2rad(float(data['destination']['theta'])))
-    print(data)
-    req.post(url="http://192.168.0.27:8080/go-to-pathfinder", json=data)
-    send_response = make_response(jsonify(data), 200)
-    send_response.headers['Access-Control-Allow-Origin'] = "*"
+    payload = validate_payload(request.json)
+    robot_confirm = relay_to_robot("/go-to-pathfinder", payload).json()
+    send_response = make_response(jsonify(payload), 200)
     return send_response
